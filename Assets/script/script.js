98b65c0b3b3e31a173f11ff71011c1f1a8273b6f
HardikShah563 <hardikts@gmail.com>
2023-03-30 16:47:23 +0530

    navbar complete

---
+++ Assets/script/script.js
@@ -0,0 +1,5 @@
+var navLines = document.querySelector(".nav-lines");
+navLines.addEventListener("click", () => {
+    var navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
+})
